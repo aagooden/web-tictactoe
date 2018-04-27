667f18c14b68dd05ca3b5d87eb62831e1f7edc52
Aaron Gooden <aagooden@me.com>
2018-04-26 23:31:59 -0400

    Fixed random and sequential.  Added winning move ability to random and sequential.

--- tictactoe_methods.rb
+++ tictactoe_methods.rb
@@ -4,9 +4,12 @@ class Game
 
     def initialize
         @board = Board.new
-				puts "The initial state of the board is #{@board.state}"
-				c = gets.chomp
         system("clear")
+
+        puts "How many players?"
+        @number_of_players = gets.chomp
+
+
 
         puts "Hello player 1.  What is your name?"
         player1 = gets.chomp
@@ -15,14 +18,32 @@ class Game
         puts ""
         @player1 = Player.new(player1, "X", @board)
 
-        puts "Hello player 2.  What is your name?"
-        player2 = gets.chomp
-        @player2 = Computer.new(player2, "O", @board)
-        puts "Hello #{player2}, you will be O's"
-        puts ""
-        puts "Press Enter to start the game"
-        gets.chomp
-        @turn = true
+        if @number_of_players == "2"
+
+          puts "Hello player 2.  What is your name?"
+          player2 = gets.chomp
+          # @player2 = Computer.new(player2, "O", @board)
+          @player2 = Player.new(player2, "O", @board)
+          puts "Hello #{player2}, you will be O's"
+          puts ""
+          puts "Press Enter to start the game"
+          gets.chomp
+          @turn = true
+        else
+          player2 = "Computer"
+          @player2 = Computer.new(player2, "O", @board)
+          puts "The Computer will be O's"
+          puts ""
+          puts "Press Enter to start the game"
+          gets.chomp
+          @turn = true
+
+          puts "What level of difficlty would you like?"
+          puts "1 = Random Opponent"
+          puts "2 = Sequential Opponent"
+          puts "3 = Unbeatable Opponent"
+          @difficulty = gets.chomp
+        end
 
     end
 
@@ -37,12 +58,20 @@ class Game
             end
 
             loop do
-							p "The class of player is #{player.class}"
+							# p "The class of player is #{player.class}"
 											if player.class == Computer
-												position = player.move(@board.state)
-												puts "The computers move is #{position}"
-												z=gets.chomp
-											else
+                        # puts "The difficulty level is #{@difficulty}"
+                          if @difficulty == "1"
+                            position = player.random_move(@board.state, @player2.piece, @player1.piece)
+                          elsif @difficulty == "2"
+                            position = player.sequential_move(@board.state, @player2.piece, @player1.piece)
+                          elsif @difficulty == "3"
+                            position = player.move(@board.state)
+                          end
+
+  												puts "The Computer's move is position #{position}"
+  												z=gets.chomp
+  										else
 				                puts "#{player.name}, enter the number of the position where you would like to play."
 				                position = gets.chomp
 				                position = position.to_i
@@ -110,11 +139,11 @@ class Board
 
     def change_state(piece, position)
         @state[position-1] = piece
-        puts "The state in change state is now #{@state}"
+        # puts "The state in change state is now #{@state}"
     end
 
     def check(position)
-        if @state[position -1] == "X" || @state[position -1] == "Y"
+        if @state[position -1] == "X" || @state[position -1] == "O"
             false
         else
             true
@@ -127,7 +156,7 @@ class Board
 
     def render
         p = @state
-        # system("clear")
+        system("clear")
         puts ''
         puts ''
         puts ''
@@ -210,89 +239,94 @@ end
 class Computer < Player
 
   def winning_move(overall_status,move)
-    puts "WINNING MOVE"
+    # puts "WINNING MOVE"
     overall_status.each do |temp|
       #check for winning move and play winning move
       if temp.count(self.piece) == 2
-        puts "This is group from to win function #{temp}"
+        # puts "This is group from to win function #{temp}"
         group = temp - [@opponent_piece, piece]
-        puts "Here is the move from winning function #{group}"
+        # puts "Here is the move from winning function #{group}"
         if group.empty?
         else
           move = group[0]
         end
       end
     end
-    puts "Move from winning_move is #{move}"
+    # puts "Move from winning_move is #{move}"
     return move
   end
 
 
   def block(overall_status,move)
-    puts "BLOCK"
+    # puts "BLOCK"
     group = []
     #check for opponent winning position and block
     overall_status.each do |temp|
-    if temp.count("X") == 2
-        puts "Here is overall_status #{overall_status}"
-        puts "Here is the group before deleting #{temp}"
+    if temp.count(@opponent_piece) == 2
+        # puts "Here is overall_status #{overall_status}"
+        # puts "Here is the group before deleting #{temp}"
+        # puts "Piece in block function is #{piece}"
+        # puts "@opponent_piece in block function is #{@opponent_piece}"
         group = temp - [@opponent_piece, self.piece]
-        puts "Here is the group from blocking function #{group}"
+        # puts "Here is the group from blocking function #{group}"
         if group.empty?
         else
           move = group[0]
         end
       end
     end
-    puts "Move from block is #{move}"
+    # puts "Move from block is #{move}"
     return move
   end
 
 
   def fork_move(overall_status, move, piece, opponent_piece)
-    puts "FORK_MOVE"
+    # puts "FORK_MOVE"
+    # puts "Here is move right after entering fork_move function #{move}"
     forks = []
     positions = []
     possible =[]
-    puts "Here is overall_status in fork_move #{overall_status}"
-    puts "Here is the piec I'm deleting this cycle #{piece}"
-    puts "The other piece is #{opponent_piece}"
+    # puts "Here is overall_status in fork_move #{overall_status}"
+    # puts "Here is the piec I'm deleting this cycle #{piece}"
+    # puts "The other piece is #{opponent_piece}"
     overall_status.each do |temp|
       if temp.count(piece) == 1 && temp.include?(opponent_piece) == false
         group = temp - [piece]
           forks.push(group)
       end
     end
-    puts "Here is the temp array from fork move #{forks}"
+    # puts "Here is the temp array from fork move #{forks}"
 
     forks.each do |element|
       element.each do |n|
         positions.push(n)
       end
     end
-    puts "Here are the positions in fork_move #{positions}"
+    # puts "Here are the positions in fork_move #{positions}"
 
     #construct an array of possible fork moves
     for x in (1..9) do
       num = positions.count(x)
       if num > 1
-        possible.push(num)
+        possible.push(x)
       end
     end
+    # puts "Here are the possible fork moves #{possible}"
 
     if possible.empty?
-      puts "Here is the move from fork_move #{move}"
+      # puts "Here is the move from fork_move #{move}"
       return move
     else
-      puts "Here is the move form fork_move #{possible.first}"
-      return possible.first
+      move = possible.first
+      # puts "Here is the move form fork_move #{move}"
+      return move
     end
 
   end
 
 
   def create_two_in_a_row(overall_status, move, piece, opponent_piece)
-    puts "CREATE_TWO_IN_A_ROW"
+    # puts "CREATE_TWO_IN_A_ROW"
     possible_two_in_a_row = []
     positions = []
     #if there is an empty location that creates a two-in-a row for me(thus forcing opponent to block), play in that location
@@ -318,27 +352,27 @@ class Computer < Player
   end
 
   def fork_block(overall_status, move, piece, opponent_piece)
-    puts "FORK_BLOCK"
+    # puts "FORK_BLOCK"
     forks = []
     positions = []
     possible =[]
-    puts "Here is overall_status in fork_block #{overall_status}"
-    puts "Here is the piec I'm deleting this fork_block cycle #{opponent_piece}"
-    puts "The other piece is #{piece}"
+    # puts "Here is overall_status in fork_block #{overall_status}"
+    # puts "Here is the piec I'm deleting this fork_block cycle #{opponent_piece}"
+    # puts "The other piece is #{piece}"
     overall_status.each do |temp|
       if temp.count(opponent_piece) == 1 && temp.include?(piece) == false
         group = temp - [opponent_piece]
           forks.push(group)
       end
     end
-    puts "Here is the temp array from fork_block #{forks}"
+    # puts "Here is the temp array from fork_block #{forks}"
 
     forks.each do |element|
       element.each do |n|
         positions.push(n)
       end
     end
-    puts "Here are the positions in fork_block #{positions}"
+    # puts "Here are the positions in fork_block #{positions}"
 
     #construct an array of possible fork moves...possible fork moves are any repeats in the array
     for x in (1..9) do
@@ -349,21 +383,21 @@ class Computer < Player
       end
 
     end
-      puts "Here are the possible fork block moves #{possible}"
+      # puts "Here are the possible fork block moves #{possible}"
 
     if possible.length == 1
       #If there is only one possible fork for the opponent, the player should block it.
       move = possible.first
-      puts "This is move from possible.first #{move}"
+      # puts "This is move from possible.first #{move}"
       return move
     elsif possible.length > 1
       # Otherwise, the player should block any forks in any way that simultaneously allows them to create two in a row...
       #the following function call finds possible two_in_a_row moves
       two_in_a_row = create_two_in_a_row(overall_status, move, piece, opponent_piece)
-      puts "Here are the two_in_a_row moves #{two_in_a_row}"
-      puts "Here is move after two_in_a_row is called #{move}"
+      # puts "Here are the two_in_a_row moves #{two_in_a_row}"
+      # puts "Here is move after two_in_a_row is called #{move}"
     else
-      puts "Here is move from fork_block...should be nothing #{move}"
+      # puts "Here is move from fork_block...should be nothing #{move}"
       return move
     end
 
@@ -383,11 +417,11 @@ class Computer < Player
     #delete the fork_block moves from the possible moves
     possible.each do |num|
       if two_in_a_row.include?(num)
-        puts "This is two_in_a_row when comparing to possible fork_block moves #{two_in_a_row}"
+        # puts "This is two_in_a_row when comparing to possible fork_block moves #{two_in_a_row}"
         move = num
         two_in_a_row = two_in_a_row - [num]
       end
-      puts "This is two_in_a_row after deleting the possible fork_block moves #{two_in_a_row}"
+      # puts "This is two_in_a_row after deleting the possible fork_block moves #{two_in_a_row}"
     end
 
     # puts "This is move #{move}"
@@ -395,15 +429,15 @@ class Computer < Player
     #   return move
     # end
 
-    puts "This is two_in_a_row without possible fork_block moves #{two_in_a_row}"
+    # puts "This is two_in_a_row without possible fork_block moves #{two_in_a_row}"
     move = two_in_a_row.sample
     return move
 
   end
 
   def play_center(board,move)
-    puts "PLAY_CENTER"
-    puts "This is the board from play_center #{board}"
+    # puts "PLAY_CENTER"
+    # puts "This is the board from play_center #{board}"
     if board[4] == 5
       move = 5
     end
@@ -411,65 +445,123 @@ class Computer < Player
   end
 
   def play_opposite_corner(board,move, piece, opponent_piece)
-    puts "PLAY_OPPOSITE_CORNER"
+    # puts "PLAY_OPPOSITE_CORNER"
     corner_opposites = [[0,9],[2,7],[8,1],[6,3]]
     corner_opposites.each do |pair|
-    puts "Board corner status is #{board[pair[0]]}"
-    puts "Pair [1] is #{[pair[1]]}"
+    # puts "Board corner status is #{board[pair[0]]}"
+    # puts "Pair [1] is #{[pair[1]]}"
       if board[pair[0]] == opponent_piece && board[pair[1]-1] == pair[1]
-        puts "Board corner status is #{board[pair[0]]} and opponent_piece is #{opponent_piece}"
+        # puts "Board corner status is #{board[pair[0]]} and opponent_piece is #{opponent_piece}"
 
         move = pair[1]
       end
     end
-    puts "Move from play_opposite_corner is #{move}"
+    # puts "Move from play_opposite_corner is #{move}"
     return move
   end
 
   def empty_corner(board, move)
-    puts "EMPTY_CORNER"
+    # puts "EMPTY_CORNER"
     corners = [0, 2, 6, 8]
     corners.each do |corner|
       if board[corner] == corner + 1
         move = corner + 1
       end
     end
-    puts "This is move from empty_corner #{move}"
+    # puts "This is move from empty_corner #{move}"
     return move
   end
 
 
   def empty_edge(board, move)
-    puts "EMPTY_EDGE"
+    # puts "EMPTY_EDGE"
     edges = [1, 3, 5, 7]
     edges.each do |edge|
       if board[edge] == edge + 1
         move = edge + 1
       end
     end
-    puts "This is move form empty_edge #{move}"
+    # puts "This is move form empty_edge #{move}"
     return move
   end
-  
 
-  def random_move(board,move)
-    puts "RANDOM_MOVE"
-      board.delete(@opponent_piece)
-      board.delete(self.piece)
+
+  def random_move(board_state, piece, opponent_piece)
+    @opponent_piece = opponent_piece
+    move = ""
+    # puts "WELCOME TO RANDOM MOVE...ENJOY YOUR STAY!"
+    board = board_state
+    overall_status = []
+    combos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+      combos.each do |group|
+        temp = []
+          group.each do |position|
+            temp.push(board[position])
+          end
+          overall_status.push(temp)
+      end
+    if move == ""
+      # puts "Sending to winning move"
+      move = winning_move(overall_status,move)
+    end
+
+    # if move == ""
+    #   puts "Sending to block"
+    #   move = block(overall_status,move)
+    # end
+
+    if move == ""
+    # puts "RANDOM_MOVE"
+    # puts "This is the board before deleting pieces#{board}"
+    # puts "This is @opponent_piece #{opponent_piece}"
+    # puts "This is self.piece #{piece}"
+      board = board - [opponent_piece]
+      board = board - [piece]
+      # puts "This is board after deleting pieces #{board}"
       move = board.sample
-      puts "This is move inside computer #{move}"
-      puts "This is the board state #{board}"
-      puts "This is @board #{@board.state}"
-        z=gets.chomp
-    # puts "What do you want computer move to be?"
-    # move = gets.chomp
-    # move = move.to_i
-    puts "Move coming form random is #{move}"
+      # puts "This is move inside computer #{move}"
+      # puts "Move coming form random is #{move}"
+    end
+    return move
+  end
+
+
+  def sequential_move(board_state, piece, opponent_piece)
+      @opponent_piece = opponent_piece
+    move = ""
+    # puts "WELCOME TO SEQUENTIAL MOVE...ENJOY YOUR STAY!"
+    board = board_state
+    overall_status = []
+    combos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+      combos.each do |group|
+        temp = []
+          group.each do |position|
+            temp.push(board[position])
+          end
+          overall_status.push(temp)
+      end
+    if move == ""
+      # puts "Sending to winning move"
+      move = winning_move(overall_status,move)
+    end
+
+    if move == ""
+      # puts "RANDOM_MOVE"
+      board = board_state
+      board = board - [opponent_piece]
+      board = board -[piece]
+        move = board.first
+        # puts "This is move inside computer #{move}"
+        # puts "This is the board state #{board}"
+        # puts "This is @board #{@board.state}"
+        # puts "Move coming form sequential is #{move}"
+    end
     return move
   end
 
 
   def move(board_state)
+    # puts "HITTING THE MOVE FUNCTION"
     move = ""
     board = Array.new
     if self.piece == "O"
@@ -493,59 +585,65 @@ class Computer < Player
           overall_status.push(temp)
       end
 
-    # These method calls, call each individual method that makes up the "unbeatable" strategy for the computer
+
     # The results of these calls are actually in the methods array...not the method calls themselves.
     # This eliminates multiple if statements.
-    methods = [
-      winning_move(overall_status, move),
-      block(overall_status, move),
-      fork_move(overall_status, move, self.piece, @opponent_piece),
-      fork_block(overall_status, move, self.piece, @opponent_piece),
-      play_center(board,move),
-      play_opposite_corner(board, move, self.piece, @opponent_piece),
-      empty_corner(board, move)
-              ]
-      methods.each do |m|
-        if m == ""
-          puts "#{m} returns nothing"
-        else
-          puts "#{m} returns something"
-          move = m
-          break
-        end
-      end
+    # methods = [
+    #   winning_move(overall_status, move),
+    #   block(overall_status, move),
+    #   fork_move(overall_status, move, self.piece, @opponent_piece),
+    #   fork_block(overall_status, move, self.piece, @opponent_piece),
+    #   play_center(board,move),
+    #   play_opposite_corner(board, move, self.piece, @opponent_piece),
+    #   empty_corner(board, move)
+    #           ]
+    #   methods.each do |m|
+    #     if m == ""
+    #       puts "#{m} returns nothing"
+    #     else
+    #       puts "#{m} returns something"
+    #       move = m
+    #       break
+    #     end
+    #   end
 
-    puts "Here is the final move return #{move}"
+
+          # These method calls, call each individual method that makes up the "unbeatable" strategy for the computer
+          if move == ""
+            move = winning_move(overall_status, move)
+          end
+
+          if move == ""
+            move = block(overall_status, move)
+          end
+
+          if move == ""
+            move = fork_move(overall_status, move, self.piece, @opponent_piece)
+          end
+
+          if move == ""
+            move = fork_block(overall_status, move, self.piece, @opponent_piece)
+          end
+
+          if move == ""
+            move = play_center(board,move)
+          end
+
+          if move == ""
+            move = play_opposite_corner(board, move, self.piece, @opponent_piece)
+          end
+
+          if move == ""
+            move = empty_corner(board, move)
+          end
+
+          if move == ""
+            move = empty_edge(board,move)
+          end
+
+    # puts "Here is the final move return #{move}"
     return move
-
-
   end
-
-
-
-  # def random_move
-  #   board = []
-  #
-	# 	board_state = @board.state
-  #   p @board.state
-  #   #if the following routine is not performed, any changes in a variable set to @board.state will subsequently change @board.state...
-  #   # This is essentially insulating @board.state from changes
-  #   board_state.each do |position|
-  #     board.push(position)
-  #   end
-  #   p board
-  #
-  #   board.delete("X")
-  #   board.delete("O")
-  #   move = board.sample
-  #   puts "This is move inside computer class #{move}"
-  #   puts "This is the cheese state #{board_state}"
-  #   puts "This is @board #{@board.state}"
-  #
-	# 		z=gets.chomp
-	# 	return move
-  #
-	# end
 
 end
 
