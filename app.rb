65bf5276b76f0a5f6729da200468e91e423c6415
Aaron Gooden <aagooden@me.com>
2018-05-02 16:22:10 -0400

    Got rid of global variable.

--- app.rb
+++ app.rb
@@ -25,8 +25,8 @@ post '/play' do
 end
 
 get '/game' do
-	$game = Game.new(session[:number_of_players], session[:player1], session[:player2], session[:difficulty])
-	session[:board_state] = $game.board_state
+	session[:game] = Game.new(session[:number_of_players], session[:player1], session[:player2], session[:difficulty])
+	session[:board_state] = session[:game].board_state
 	puts "Here is the board_state you were looking for #{session[:board_state]}"
 	redirect "/again"
 	# erb :board
@@ -36,17 +36,17 @@ get '/move' do
 	current_move = params[:move]
 
 	#Check to see if current_move has already been played...if so, redirect back to board for another try
-	puts "#{$game.board_state.include?(current_move)}"
-	if $game.board_state.include?(current_move.to_i) == false
+	puts "#{session[:game].board_state.include?(current_move)}"
+	if session[:game].board_state.include?(current_move.to_i) == false
 		puts "GO TO BOARD"
 		redirect "/no_dice"
 	end
 
 	puts "Step 2, send move to game.play"
-	player = $game.play(current_move)
+	player = session[:game].play(current_move)
 	puts "Step 3, returns the array depending on win, tie, none...array is #{player}"
-	puts "Step 4, update board_state with the game board state which is #{$game.board_state}"
-	session[:board_state] = $game.board_state
+	puts "Step 4, update board_state with the game board state which is #{session[:game].board_state}"
+	session[:board_state] = session[:game].board_state
 
 #If last move created a winner or a tie, redirect to /winner
 	if player[5] == "winner" || player[5] == "tie"
@@ -61,12 +61,12 @@ get '/move' do
 
 
 	if player[0] == "Computer"
-	elsif player[0] != "Computer" && $game.player2_class == Player
+	elsif player[0] != "Computer" && session[:game].player2_class == Player
 		puts "This is where player2 is human and NOT the computer...should go to erb :board at this point"
 		erb :board
 	else
 		puts "If player[0] was not equal Computer, go back through game.play with current move"
-		player = $game.play(current_move)
+		player = session[:game].play(current_move)
 		erb :board
 	end
 
@@ -112,10 +112,10 @@ post "/again" do
 	first_move = params[:first_move]
 
 	# Check that player2 is the Computer AND player2 is supposed to go first in the next game
-	if $game.player2_class == Computer && first_move == "player2"
-		$game.play_again(false)
-		session[:board_state] = $game.board_state
-		$game.play(10) #10 is just a placeholder for calling $game.play on the computer's turn
+	if session[:game].player2_class == Computer && first_move == "player2"
+		session[:game].play_again(false)
+		session[:board_state] = session[:game].board_state
+		session[:game].play(10) #10 is just a placeholder for calling session[:game].play on the computer's turn
 		erb :board
 	else
 		if first_move == "player2"
@@ -123,8 +123,8 @@ post "/again" do
 		else
 			turn = true
 		end
-		$game.play_again(turn)
-		session[:board_state] = $game.board_state
+		session[:game].play_again(turn)
+		session[:board_state] = session[:game].board_state
 		erb :board
 	end
 end
